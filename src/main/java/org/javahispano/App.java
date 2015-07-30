d148088b0bed7105383ddd4cea56cd9a6825b578
cursoip2015 <cursoipartek2015@gmail.com>
2015-07-30 10:43:25 +0200

    initial import

---
+++ src/main/java/org/javahispano/App.java
@@ -0,0 +1,13 @@
+package org.javahispano;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
